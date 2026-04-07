c67f7fd12a0981e93b9d31515f1b20d12af9cb08
Fortovaya <Fortovaya89@bk.ru>
2026-04-07 22:48:44 +0300

    [SW-3] Добавлена модель DropdownStyle для стилизации dropdown

---
+++ Swaply/UI Components/Dropdown/Model/DropdownStyle.swift
@@ -0,0 +1,17 @@
+//
+//  DropdownStyle.swift
+//  Swaply
+//
+//  Created by Алина on 07.04.2026.
+//
+
+import UIKit
+
+struct DropdownStyle {
+	let titleFont: UIFont
+	let titleColor: UIColor
+	let dropDownBackgroundColor: UIColor
+	let placeholderFont: UIFont
+	let placeholderColor: UIColor
+	let arrowTintColor: UIColor
+}
