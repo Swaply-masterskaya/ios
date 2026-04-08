c7e4b4ef5cc650bb4509f7bb8d2efda38767312c
Fortovaya <Fortovaya89@bk.ru>
2026-04-08 14:30:08 +0300

    [SW-3] Добавлена верстка DropdownCell для DropdownItem

---
+++ Swaply/UI Components/Dropdown/Componentss/DropdownCell.swift
@@ -0,0 +1,78 @@
+//
+//  DropdownCell.swift
+//  Swaply
+//
+//  Created by Алина on 08.04.2026.
+//
+
+import UIKit
+import SnapKit
+
+final class DropdownCell: UITableViewCell {
+	static let reuseIdentifier = "DropdownCell"
+
+	private(set) lazy var checkboxImageView: UIImageView = {
+		let imageView = UIImageView()
+		imageView.contentMode = .scaleAspectFit
+		imageView.layer.cornerRadius = 3
+		imageView.layer.masksToBounds = true
+		return imageView
+	}()
+
+	private(set) lazy var valueLabel: UILabel = {
+		let label = UILabel()
+		label.font = AppTypography.body
+		label.textColor = AppColors.black
+		return label
+	}()
+
+	private(set) lazy var cellContent: UIStackView = {
+		let cellContent = UIStackView(arrangedSubviews: [checkboxImageView, valueLabel])
+		cellContent.axis = .horizontal
+		cellContent.spacing = 8
+		cellContent.alignment = .center // просмотреть вариант с fill
+		return cellContent
+	}()
+
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		setupConstraints()
+		selectionStyle = .none
+	}
+
+	required init?(coder: NSCoder) {
+		assertionFailure("init(coder:) has not been implemented")
+		return nil
+	}
+
+	private func setupConstraints() {
+		contentView.clipsToBounds = true
+		contentView.addSubview(cellContent)
+
+		cellContent.snp.makeConstraints {
+			$0.leading.equalToSuperview().inset(12)
+			$0.trailing.equalToSuperview().inset(12)
+			$0.centerY.equalToSuperview()
+		}
+
+		checkboxImageView.snp.makeConstraints {
+			$0.width.equalTo(24)
+			$0.height.equalTo(24)
+		}
+
+		contentView.backgroundColor = .clear
+	}
+
+	func configureCell(text: String, isSelected: Bool = false) {
+		valueLabel.text = text
+		updateCheckBox(isSelected: isSelected)
+	}
+
+	private func updateCheckBox(isSelected: Bool) {
+		let imageName = isSelected ? "checkmark.square.fill" : "square"
+		let image = UIImage(systemName: imageName)?.withRenderingMode(.alwaysTemplate)
+		checkboxImageView.image = image
+		checkboxImageView.image = UIImage(systemName: imageName)
+		checkboxImageView.tintColor = isSelected ? AppColors.secondaryOrange : AppColors.grey400
+	}
+}
