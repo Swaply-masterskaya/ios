cd244cd65842306883e78cbb592f9eee2df0a6d3
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2026-04-14 12:29:26 +0300

    [SW-28] - добавлен splashScreen

---
+++ Swaply/SplashViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SplashViewController.swift
+//  Swaply
+//
+//  Created by Alina on 14/04/2026.
+//
+
+import UIKit
+import SnapKit
+
+final class SplashViewController: UIViewController {
+
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(resource: .logo))
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = AppColors.backgroundPrimary
+        view.addSubview(logoImageView)
+
+        logoImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-53)
+            make.width.height.equalTo(80)
+        }
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+            (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?
+                .showOnboarding()
+        }
+    }
+}
