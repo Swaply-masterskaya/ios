82491841a85e47067e425a3e6c8454b961366372
Alexey Nepryakhin <alnepryakhin@gmail.com>
2026-04-21 18:44:11 +0300

    [SW-10-11] - Создал управляемую секцию ProgressBar'а

---
+++ Swaply/UI Components/ProgressBar/ProgressSection.swift
@@ -0,0 +1,64 @@
+import UIKit
+import SnapKit
+
+class ProgressSection: UIView {
+
+    private let progressView = ProgressView()
+
+    override var intrinsicContentSize: CGSize {
+        CGSize(width: UIView.noIntrinsicMetric, height: 8)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) { nil }
+
+    private func setupUI() {
+        backgroundColor = AppColors.grey600
+        layer.cornerRadius = AppRadius.extraExtraSmall
+        layer.masksToBounds = true
+
+        addSubview(progressView)
+
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        progressView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        setWidthToZero()
+    }
+
+    func setWidthToZero() {
+        UIView.animate(withDuration: 0.3) {
+            self.progressView.snp.remakeConstraints {
+                $0.width.equalToSuperview().multipliedBy(0)
+            }
+            self.layoutIfNeeded()
+        }
+    }
+
+    func setWidthToHalf() {
+        UIView.animate(withDuration: 0.3) {
+            self.progressView.snp.remakeConstraints {
+                $0.width.equalToSuperview().multipliedBy(0.5)
+            }
+            self.layoutIfNeeded()
+        }
+    }
+
+    func setWidthToFull() {
+        UIView.animate(withDuration: 0.3) {
+            self.progressView.snp.remakeConstraints {
+                $0.width.equalToSuperview().multipliedBy(1)
+            }
+            self.layoutIfNeeded()
+        }
+    }
+}
