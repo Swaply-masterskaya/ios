d533195419262a8ae7c8d0285f5bae5e5eec0b55
mono1it <host1425@gmail.com>
2026-04-25 17:52:59 +0300

    feature/shcherbakov/SW-40-Добавил обработку нажатий кнопок

--- Swaply/Authorization/AuthorizationViewController.swift
+++ Swaply/Authorization/AuthorizationViewController.swift
@@ -18,12 +18,18 @@ final class AuthorizationViewController: UIViewController {
         return view
     }()
     private lazy var registerButton: UIButton = makeButton(
-        title: registerText,
-        color: AppColors.buttonDefaultNormal
+        title: Resources.WelcomeScreen.registerButtonTitle,
+        color: AppColors.buttonDefaultNormal,
+        action: UIAction { [weak self] _ in
+            self?.viewModel.registerButtonTapped()
+        }
     )
     private lazy var entryButton: UIButton = makeButton(
-        title: entryText,
-        color: AppColors.buttonDefaultDisabledTypography
+        title: Resources.WelcomeScreen.enterButtonTitle,
+        color: AppColors.buttonDefaultDisabledTypography,
+        action: UIAction { [weak self] _ in
+            self?.viewModel.entryButtonTapped()
+        }
     )
     private lazy var stackOfButtons: UIStackView = {
         let stack = UIStackView()
@@ -36,16 +42,13 @@ final class AuthorizationViewController: UIViewController {
     }()
     // MARK: - Constants
     private let viewModel: AuthorizationViewModel
-    private let registerText: String = "Зарегестрироваться"
-    private let entryText: String = "Войти"
     // MARK: - Initializers
     init(viewModel: AuthorizationViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    @available(*, unavailable)
+    required init?(coder: NSCoder) { nil }
     // MARK: - Internal Methods
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -85,13 +88,18 @@ final class AuthorizationViewController: UIViewController {
             make.bottom.equalToSuperview().inset(AppSpacing.xxlarge)
         }
     }
-    private func makeButton(title: String, color: UIColor) -> UIButton {
+    private func makeButton(
+        title: String,
+        color: UIColor,
+        action: UIAction
+    ) -> UIButton {
         let button = UIButton(type: .system)
         button.setTitle(title, for: .normal)
         button.setTitleColor(AppColors.white, for: .normal)
         button.titleLabel?.font = AppTypography.accentButtonTitle
         button.backgroundColor = color
         button.layer.cornerRadius = AppRadius.extraLarge
+        button.addAction(action, for: .touchUpInside)
         return button
     }
 }
--- Swaply/Authorization/AuthorizationViewModel.swift
+++ Swaply/Authorization/AuthorizationViewModel.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 final class AuthorizationViewModel {
-    
+    func entryButtonTapped() {
+        print("entryButtonTapped")
+    }
+    func registerButtonTapped() {
+        print("registerButtonTapped")
+    }
 }
