ab8d7fd0e410fc84f63d8f7f9e6bbafc40d8886f
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2026-04-29 11:22:23 +0300

    [SW-30] - удален лишний switch

--- Swaply/UI Components/Helpers/VisualEffectBadgeView.swift
+++ Swaply/UI Components/Helpers/VisualEffectBadgeView.swift
@@ -7,50 +7,45 @@
 import UIKit
 
 final class VisualEffectBadgeView: UIView {
-
+    
     enum CornerStyle {
         case circle
         case pill
     }
-
+    
     let contentView = UIView()
-
+    
     private let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterial))
     private let cornerStyle: CornerStyle
     private let overlayColor: UIColor
-
+    
     init(cornerStyle: CornerStyle, overlayColor: UIColor = .clear) {
         self.cornerStyle = cornerStyle
         self.overlayColor = overlayColor
         super.init(frame: .zero)
         setupUI()
     }
-
+    
     @available(*, unavailable)
     required init?(coder: NSCoder) { nil }
-
+    
     override func layoutSubviews() {
         super.layoutSubviews()
-        switch cornerStyle {
-        case .circle:
-            layer.cornerRadius = bounds.height / 2
-        case .pill:
-            layer.cornerRadius = bounds.height / 2
-        }
+        layer.cornerRadius = bounds.height / 2
     }
-
+    
     private func setupUI() {
         layer.masksToBounds = true
         addSubview(blurView)
         addSubview(contentView)
-
+        
         blurView.alpha = 0.4
         contentView.backgroundColor = overlayColor
-
+        
         blurView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-
+        
         contentView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
